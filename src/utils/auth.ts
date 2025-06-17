eee1dd9e4d5a859e3e51c20c59d389304bd065e6
cifu0426 <alejandro.cifuentes@udea.edu.co>
2025-06-17 05:23:45 -0500

    corrijo auth.ts

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,9 +1,11 @@
 // Guarda el username en localStorage
 export function saveUsernameToStorage(username: string) {
-  localStorage.setItem("username", username)
+  if (typeof window !== "undefined") {
+    localStorage.setItem("username", username);
+  }
 }
 
 // Obtiene el username desde localStorage
 export function getUsernameFromStorage() {
-  return typeof window !== "undefined" ? localStorage.getItem("username") : null
+  return typeof window !== "undefined" ? localStorage.getItem("username") : null;
 }
